b3a4b2e272bbe280ec35076afdd5e8d0b0dbc115
wanvan10 <55166348+wanvan10@users.noreply.github.com>
2024-11-06 00:32:00 +0000

    jogo da velha

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jogo da Velha</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+    <body>
+        <h1>Jogo da Velha</h1>
+        <div id= "tabuleiro">
+        <div class="casa"></div>
+        <div class="casa"></div>
+        <div class="casa"></div>
+        <div class="casa"></div>
+        <div class="casa"></div>
+        <div class="casa"></div>
+        <div class="casa"></div>
+        <div class="casa"></div>
+        <div class="casa"></div>
+        </div>
+    </body>
+</html>
